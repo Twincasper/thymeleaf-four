9cca3cfd21688735cab0b097db4f7b6be84b81a0
Twincasper <justinwatson651@gmail.com>
2024-12-25 16:21:59 -0500

    Initial commit

---
+++ src/main/java/com/example/thymeleaffour/ThymeleafFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaffour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleaffour/ThymeleafFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaffour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
